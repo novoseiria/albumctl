e261ae066f7592353996a87509df84387ed97fb7
Nile Jocson <novoseiria@gmail.com>
2026-07-26 02:18:15 +0800

    Add AlbumManifest

---
+++ src/album.rs
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <novoseiria@gmail.com>
+// SPDX-License-Identifier: MPL-2.0
+
+use serde::Deserialize;
+
+use crate::tracklist::Disc;
+
+
+
+#[derive(Debug, Deserialize)]
+pub struct AlbumManifest {
+	title: String,
+	artist: String,
+	year: u16,
+
+	discs: Vec<Disc>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <novoseiria@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+mod album;
 mod config;
 mod library;
 mod tracklist;
